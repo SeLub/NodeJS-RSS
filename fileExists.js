5ad7d4a605b35f5d2b69de67fb6468f76e0bab73
Sergey Lubimov <siteup24@gmail.com>
2021-11-11 00:44:09 +0300

    fix: refactor and fix error handling

--- fileExists.js
+++ fileExists.js
@@ -4,27 +4,25 @@ const checkFileExists = (file, constants) => {
 
   let fileExists;
 
-fs.access(file, constants, (err) => {
-  
-  if (err) {
+  try {
+    
+    fs.accessSync(file, constants);
 
-    console.error(`File ${file} does not exist or does not has right permissions`);
+    console.log(`File ${file} exists and has right permissions`);
 
-	fileExists = false;
-  
-  } else {
+    fileExists = true;
 
-  	let substr = (constants.toString().includes('R_OK')) ? 'for reading' : (constants.toString().includes('W_OK')) ? 'for writing' : 'has right permissions';
+  } catch (error) {
 
-    console.log(`File ${file} exists and has permissions ${substr}`);
-	
-	fileExists = true;
-}});
+  	console.log(`File ${file} does not exist or does not has right permissions`);
+
+    fileExists = false;
+  }
 
 return fileExists;
 
 }
 
-//checkFileExists('enigma2.js', fs.constants.F_OK | fs.constants.R_OK)
+//checkFileExists('coders.js', fs.constants.F_OK | fs.constants.R_OK)
 
 module.exports = { checkFileExists };
