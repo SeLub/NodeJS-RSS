5b898854ebdb9ca66a52490169ccb3c6011a1304
Sergey Lubimov <siteup24@gmail.com>
2021-11-11 10:37:36 +0300

    feat: atbashCoder 100% ready tested

--- coders/atbashCoder.js
+++ coders/atbashCoder.js
@@ -1,8 +1,33 @@
-const atbashCoder = (char) => {
+const atbashCoder = (char) => {    
 
-	return 'A';
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var tebahpla = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+    var alphabet1 = "abcdefghijklmnopqrstuvwxyz";
+    var tebahpla1 = "zyxwvutsrqponmlkjihgfedcba";
+    var decoded_string = "";
+
+    for (var i = 0; i < char.length; i++) {
+        var coded_letra = char.charAt(i);
+        
+	if (/[^a-zA-Z]/.test(char[i])) {
+		decoded_string = decoded_string+char[i];	
+	}
+	else if (char[i] === char[i].toUpperCase()) {
+	    	var letraPosMayus = alphabet.indexOf(coded_letra);
+	    	var tebLetraPosMayus = tebahpla.charAt(letraPosMayus);
+	    	decoded_string = decoded_string+tebLetraPosMayus;
+        } else {
+	    	var letraPosMinus1 = alphabet1.indexOf(coded_letra);
+	    	var tebLetraPosMinus1 = tebahpla1.charAt(letraPosMinus1);
+	    	decoded_string = decoded_string + tebLetraPosMinus1;
+        }
+
+    }
+    return decoded_string;
 }
 
 module.exports = {
 	atbashCoder
 };
+
+console.log(atbashCoder('A'));
