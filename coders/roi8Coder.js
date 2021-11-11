7e10e13360f38cc679b7d4cf2d92c637437e7534
Sergey Lubimov <siteup24@gmail.com>
2021-11-11 11:07:49 +0300

    feat: roi8Coder 100% ready & tested

--- coders/roi8Coder.js
+++ coders/roi8Coder.js
@@ -1,8 +1,15 @@
-const rot8Coder = (char) => {
-
-	return 'R';
-}
+const rot8Coder = (char, type = true) =>      {
+        
+        if (!char.match(/[A-Za-z]/)) return char;
+        
+        c = Math.floor(char.charCodeAt(0) / 97);
+        
+        type ? k = (char.toLowerCase().charCodeAt(0) - 96) % 26 + 13 : k = (char.toLowerCase().charCodeAt(0) - 96) % 26 - 13;
+        
+        return String.fromCharCode(k + ((c == 0) ? 64 : 96));
+     }
 
 module.exports = {
+
 	rot8Coder
 };
